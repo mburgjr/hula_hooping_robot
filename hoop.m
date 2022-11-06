bfa3271c4e61b66dada8fbc5181af4dea36303ab
Michael Burgess <mburgjr@mit.edu>
2022-11-06 14:17:04 -0500

    Got the circle kind of working

--- hoop.m
+++ hoop.m
@@ -10,8 +10,10 @@ R_person = 0.01; % m
 mu = 0.1; % [/]
 
 % Elliptical input
-a = R_hoop/2; % m       Length of x-axis limits
-b = R_hoop/2; % m       Length of y-axis limits
+R_traj_person = R_hoop/2;
+R_traj_hoop = R_traj_person - R_person + R_hoop;
+a = R_traj; % m       Length of x-axis limits
+b = R_traj; % m       Length of y-axis limits
 dth = 2*pi; % rad/sec   Traversal speed
 
 %% Forward simulation
@@ -28,70 +30,41 @@ p_person = [a*cos(ang);     b*sin(ang)];            % [x, y] X N
 v_person = [-a*sin(ang);    b*cos(ang)]*dth;        % [dx, dy] X N
 a_person = [-a*cos(ang);    -b*sin(ang)]*dth*dth;   % [ddx, ddy] X N
 
-
-
-
-
-% State
-
-% [th, r, dth, dr] of hoop
-
-% Start here:
-% Fix contact point
-% Choose force within friction cone
-
-
-
-
 % Init hoop states
 p_hoop = zeros([3 N]);      % [x, y, phi] x N
 v_hoop = zeros([3 N]);      % [dx, dy, dphi] x N
 
 % Set initial state of hoop
-p_hoop(1:2, 1) = p_person(:,1) + [R_person - R_hoop; 0];
+p_hoop(1:2, 1) = p_person(:,1) + [-R_person + R_hoop; 0];
+p_hoop(3, 1) = pi;
+v_hoop(2, 1) = p_hoop(1,1)*dth;
+v_hoop(3, 1) = dth;
+
+% Save contact point for plotting
+p_contact = zeros([2 N]);
+
+% Centripetal force required
+R = R_traj_person - R_person + R_hoop;
+T = 2*pi/dth;
+F_centripetal = 4*pi^2*m_hoop*R/(T^2);
 
 % Iterate for timesteps
 for i = 1:N
-
-
-
-    % TODO: This loop is broken
-    % It should use person trajectory to update hoop positions
-    % While maintaining contact
-
-    % My idea was that the interaction point supplies some force
-    % To accelerate the hoop in the same direction as the person
-    % And, a torque is applied about the center of the hoop according to
-    % this acceleration vector of the person
-
-    % 1. I don't think this is dynamically correct
-    % 2. Torque hard :(
-
-
-
-    % Find contact point, assuming contact
-    % HP = p_person(:,i) - p_hoop(1:2,i);
-    % HP_hat = HP / norm(HP);
-    % contact_point = R_hoop*HP_hat + p_hoop(1:2,i);
-
-    % Force calc
-    % F_c = m_hoop * a_person(:,i);
-
-
+    
+    % Apply centripetal force to inside of trajectory
     phi = p_hoop(3, i);
-    F_c = [10*cos(phi); 10*sin(phi)];
+    F_c = F_centripetal*[cos(phi); sin(phi)];
 
-
+    % Save contact point
+    p_contact(:, i) = p_hoop(1:2,i) + [R_hoop*cos(phi); R_hoop*sin(phi)];
 
     % Moment calc
-    tau_c = mu*10*R_hoop;
+    tau_c = 0;
 
-
-    
     % Update hoop state
     v_hoop(1:2, i+1) = dt*F_c/m_hoop + v_hoop(1:2,i);
     v_hoop(3, i+1) = dt*tau_c/I_hoop + v_hoop(3,i);
-    p_hoop(:, i+1) = p_hoop(:,i) + dt*v_hoop(:,i+1);
+    p_hoop(:, i+1) = p_hoop(:,i) + dt*v_hoop(:,i);
 
 end
 
@@ -104,8 +77,9 @@ end
 
 % Prepare plot handles
 hold on
-h_hoop = plot([0],[0],'LineWidth',2);
-h_person = plot([0],[0],'LineWidth',2);
+h_hoop = plot([0],[0],'b-','LineWidth',2);
+h_person = plot([0],[0],'r-','LineWidth',2);
+h_contact = plot([0],[0],'r.','MarkerSize',25);
 
 xlabel('x'); ylabel('y');
 h_title = title('t=0.0s');
@@ -120,6 +94,14 @@ person_circle_y = R_person*sin(th);
 hoop_circle_x = R_hoop*cos(th);
 hoop_circle_y = R_hoop*sin(th);
 
+traj_circle_x = R_traj_person*cos(th);
+traj_circle_y = R_traj_person*sin(th);
+plot(traj_circle_x, traj_circle_y, 'k--')
+
+traj_circle_x = R_traj_hoop*cos(th);
+traj_circle_y = R_traj_hoop*sin(th);
+plot(traj_circle_x, traj_circle_y, 'k--')
+
 % For visualization purposes
 sim_speed = 0.05;
 
@@ -127,11 +109,14 @@ sim_speed = 0.05;
 for i = 1:N
     set(h_title,'String',  sprintf('t=%.2f',t(i)) ); % update title
     
-    set(h_person,'XData', person_circle_x + p_person(1,i));
-    set(h_person,'YData', person_circle_y + p_person(2,i));
+    % set(h_person,'XData', person_circle_x + p_person(1,i));
+    % set(h_person,'YData', person_circle_y + p_person(2,i));
     
     set(h_hoop,'XData', hoop_circle_x + p_hoop(1,i));
     set(h_hoop,'YData', hoop_circle_y + p_hoop(2,i));
+
+    set(h_contact,'XData', p_contact(1,i));
+    set(h_contact,'YData', p_contact(2,i));
 
     pause(dt/sim_speed)
 end
